1c70417cfda1a4a712238c5db28ce750b6895546
Desmond51 <desmondtekum9@gmail.com>
2024-02-27 14:43:45 +0100

    learned string, integers declaration inGo

--- main.go
+++ main.go
@@ -2,5 +2,11 @@ package main;
 import "fmt";
 
 func main(){
-	fmt.Println("hello Dezy")
+	//string
+	var name1 string = "Dezy";
+	var name2 string = "Chris";
+	name3:= "Baddest boy"
+	name3 = "Godwin"
+	var num4 int= 25
+	fmt.Println(name1, name2, name3, num4)
 }
